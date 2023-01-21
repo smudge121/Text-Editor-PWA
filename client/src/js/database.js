c42de70a8c0070198c6bfb47d310a801f8de6741
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-21 14:54:35 -0800

    updated client js folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { text } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({content: content});
+  const result = await request;
+  console.log('data saved to database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
